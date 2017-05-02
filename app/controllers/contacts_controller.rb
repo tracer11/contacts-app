953aaedaecca252c7388b55d99c506e22ea09b56
tracer11 <karolgurba22@gmail.com>
2017-05-01 23:22:03 -0500

    Added all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     @contact = Contact.first
     render 'contact.html.erb'
   end
+
+  def all
+    @people = Contact.all 
+    render 'all_contacts.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  get '/contacts' => 'contacts#person'
+ get '/all_contacts' => 'contacts#all'
 end
