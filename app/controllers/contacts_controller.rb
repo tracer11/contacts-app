762e20ccdffb903dc376c41e4867eea800167a8b
tracer11 <karolgurba22@gmail.com>
2017-05-10 21:28:28 -0500

    added bio

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,44 @@
 class ContactsController < ApplicationController
-  def person
-    @contact = Contact.first
-    render 'contact.html.erb'
+  def index
+    @contacts = Contact.all
   end
 
-  def all
-    @people = Contact.all 
-    render 'all_contacts.html.erb'
+  def new
+  end
+
+  def create
+    contact = Contact.new(first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          middle_name: params[:middle_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number],
+                          bio: params[:bio]
+                          )
+    contact.save
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    contact.assign_attributes(first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          middle_name: params[:middle_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number],
+                          bio: params[:bio]
+                          )
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime('%b %d, %Y')
+  end
 end
